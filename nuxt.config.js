2acbb7836cb0665e57d1d12cbe8e18f00e479dd2
Joan Reyero <joanreyero@gmail.com>
2020-08-14 08:04:54 +0100

    Added remote

--- nuxt.config.js
+++ nuxt.config.js
@@ -62,7 +62,7 @@ export default {
   */
   axios: {
     baseURL: (process.env.NODE_ENV == 'production') ?
-      '' : 'http://127.0.0.1:5000',
+      'https://matched-filters-api.herokuapp.com/' : 'http://127.0.0.1:5000',
     //headers: {
       // 'Content-Type': 'application/json',
     //  'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8'
