5f94c77c5e91c9622c51096da06eb47662a5e400
Joan Reyero <joanreyero@gmail.com>
2020-08-18 10:33:33 +0100

    Changed the camera parameters

--- components/editor.vue
+++ components/editor.vue
@@ -1,65 +1,21 @@
 <template>
   <div class="w-full relative">
     <div @click="editCamera=true"
-         class="border-2 bg-gray-200 rounded-lg w-5/6 h-16 shadow-md text-center 
-                mx-auto pt-5 cursor-pointer hover:bg-gray-300"> 
+         class="border-2 bg-gray-200 rounded-lg w-5/6 h-16 shadow-md text-center mx-auto pt-5 cursor-pointer hover:bg-gray-300"> 
       <h2> Edit camera parameters </h2>
     </div>
+    <div v-if="editCamera" class="edit-params-cont"> </div>
     <div v-if="editCamera" 
-         class="absolute left-0 right-0 top-0 mx-auto w-5/6 py-10 bg-gray-100 rounded-lg shadow-lg text-center z-50"
-         style="height: 600px"> 
+         class="absolute left-0 right-0 mx-auto w-5/6 py-10 bg-gray-100 rounded-lg shadow-lg text-center z-50">
       <div>
+        <h2 class="mb-5"> Enter camera parameters </h2>
         <h3 class="pb-3"> Resolution (px) </h3>
         </ul> 
           <li> x: <input class="input" v-model="camera.x"/> </li>
           <li> y: <input class="input" v-model="camera.y"/> </li>
         </ul>
-        <h3 class="pt-5 pb-2"> Field of view </h3>
-        <p class="font-medium"> Choose the input type </p>
-        <div class="rounded-sm border-2 bg-gray-200 w-5/6 max-w-lg mx-auto mt-2 mb-4 flex items-center">
-          <button @click="camera.fovType = 'K'"
-                  class="w-1/2 block border-r-2 h-10 focus:outline-none
-                         hover:bg-gray-300 hover:font-medium"
-                  :class="{active: camera.fovType == 'K'}"> 
-            Intrinsic Matrix 
-          </button>
-          <button @click="camera.fovType = 'fov'"
-                  class="w-1/2 block h-10 focus:outline-none
-                         hover:bg-gray-300 hover:font-medium"
-                  :class="{active: camera.fovType == 'fov'}"> 
-            Field of View 
-          </button>
-        </div>
-      </div>
-      <div v-if="camera.fovType == 'K'" class="w-full">
-        <p class="mb-3"> Enter the Intrinsic Matrix: </p>
-        <div class="matrix-row">
-          <input class="matrix border-l-2 border-t-2 border-b" 
-                 v-model="camera.fov[0]"/>
-          <input class="matrix border-t-2 border-b border-l border-r" 
-                 v-model="camera.fov[1]"/>
-          <input class="matrix border-r-2 border-t-2 border-b" 
-                 v-model="camera.fov[2]"/>
-        </div>
-        <div class="matrix-row">
-          <input class="matrix border-l-2 border-b" 
-                 v-model="camera.fov[3]"/>
-          <input class="matrix border-b border-l border-r" 
-                 v-model="camera.fov[4]"/>
-          <input class="matrix border-r-2 border-b" 
-                 v-model="camera.fov[5]"/>
-        </div>
-        <div class="matrix-row">
-          <input class="matrix border-l-2 border-b-2" 
-                 v-model="camera.fov[6]"/>
-          <input class="matrix border-b-2 border-l border-r" 
-                 v-model="camera.fov[7]"/>
-          <input class="matrix border-r-2 border-b-2" 
-                 v-model="camera.fov[8]"/>
-        </div>
-      </div>
-      <div v-else class="w-full">
-        <p class="mb-3">Enter the fields of view:</p>
+        <h3 class="pt-5 pb-2"> Field of view </h3> 
+        <p class="mb-3">Enter the fields of view in degrees:</p>
         <ul>
           <li> FOV x: <input class="input" v-model="camera.fov[0]"/> </li>
           <li> FOV y: <input class="input" v-model="camera.fov[1]"/> </li>
@@ -159,6 +115,11 @@ export default {
 </script>
 
 <style scoped>
+
+.edit-params-cont {
+  @apply absolute bg-gray-800 opacity-75 w-11/12 left-0 right-0 mx-auto top-0 z-40 rounded-lg shadow-xl;
+  height: calc(95vh - 86px)
+}
 
 input:focus {
   outline: none;
